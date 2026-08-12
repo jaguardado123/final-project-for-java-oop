ffa8243eae0d41f2f7c439e70045f0ae23d7750a
Sebastian Perez <sebastian.perez05@utrgv.edu>
2026-08-12 19:39:21 +0000

    Nothing new

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello there");
-        System.out.println("General Kenobi");
     }
 }
